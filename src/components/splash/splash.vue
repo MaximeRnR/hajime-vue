d1951015c98e98762c581d241d0a292c9ad50043
Maxime Reynier <mreynier@excilys.com>
2018-02-13 17:49:08 +0100

    add shapes and good anim with gradient

--- src/components/splash/splash.vue
+++ src/components/splash/splash.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="splash-component">
-        <div id="splash-bg">
-            <div id="splash-gradient">
+        <div id="splash-form-one">
+            <div id="splash-gradient-one">
                 <div class="title">
                     <p>Hajime is </p>
                     <ul id="qualities">
@@ -9,9 +9,42 @@
                     </ul>
                 </div>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-                <polygon fill="white" points="0,100 100,0 100,100"/>
+            <svg id="svg-one" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+                <polygon filter="url(#dropshadow)" fill="white" points="0,70 0,100 100,100 100,0 75,60 50,75"/>
+                <filter id="dropshadow" height="130%">
+                    <feGaussianBlur in="SourceAlpha" stdDeviation="4"/> <!-- stdDeviation is how much to blur -->
+                    <feOffset dx="1" dy="1" result="offsetblur"/> <!-- how much to offset -->
+                    <feComponentTransfer>
+                        <feFuncA type="linear" slope="0.5"/> <!-- slope is the opacity of the shadow -->
+                    </feComponentTransfer>
+                    <feMerge>
+                        <feMergeNode/> <!-- this contains the offset blurred image -->
+                        <feMergeNode in="SourceGraphic"/>
+                        <!-- this contains the element that the filter is applied to -->
+                    </feMerge>
+                </filter>
             </svg>
+        </div>
+        <div id="splash-form-two">
+            <div id="splash-gradient-two">
+                <svg id="svg-two" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+                    <polygon filter="url(#dropshadow-two)" fill="white" points="0,0 0,75 100,75 100,0 "/>
+                    <filter id="dropshadow-two" height="130%">
+                        <feGaussianBlur in="SourceAlpha" stdDeviation="8"/> <!-- stdDeviation is how much to blur -->
+                        <feOffset dx="1" dy="-1" result="offsetblur"/> <!-- how much to offset -->
+                        <feComponentTransfer>
+                            <feFuncA type="linear" slope="0.5"/> <!-- slope is the opacity of the shadow -->
+                        </feComponentTransfer>
+                        <feMerge>
+                            <feMergeNode/> <!-- this contains the offset blurred image -->
+                            <feMergeNode in="SourceGraphic"/>
+                            <!-- this contains the element that the filter is applied to -->
+                        </feMerge>
+                    </filter>
+                </svg>
+                <div id="splash-gradient-in" ></div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -21,34 +54,86 @@
         name: "splash.vue",
         data() {
             return {
-                qualities: ["Innovant"]
+                qualities: ["Helpful"]
             }
         }
     }
-
 </script>
 
 <style scoped>
 
     #splash-component {
-    }
-
-    #splash-bg {
-        background: #4BC0C8;
-        position: relative;
         width: 100vw;
-        height: 80vh;
-        border-radius: 5px;
-        transform: translate3d(0, 0, 0);
+        height: 100vh;
     }
 
-    #splash-gradient {
+
+    #splash-form-one {
+        position: relative;
+        top: 0;
+        width: 50%;
+        height: 60%;
+        background: #4BC0C8;
+        border-radius: 5px;
+        will-change: transform, opacity;
+        opacity: 0;
+        transform: translateY(-100%);
+        animation: 1s cubic-bezier(0.215, 0.61, 0.355, 1) 0.5s 1 slideInFromTopWithOpacity forwards;
+    }
+
+    #splash-gradient-one {
         width: 100%;
         height: 100%;
-        animation: 1s cubic-bezier(0.215, 0.61, 0.355, 1) 0s 1 slideInFromTop;
+        will-change: transform;
+        transform: translateY(-100%);
+        animation: 1s cubic-bezier(0.215, 0.61, 0.355, 1) 1s 1 slideInFromTop forwards;
         background: #FEAC5E; /* fallback for old browsers */
         background: -webkit-linear-gradient(to top, #4BC0C8, #C779D0, #FEAC5E); /* Chrome 10-25, Safari 5.1-6 */
         background: linear-gradient(to top, #4BC0C8, #C779D0, #FEAC5E); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    }
+
+    #svg-one {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        /* set height to pixels if you want angle to change with screen width */
+
+    }
+
+    #splash-form-two {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: 55%;
+        will-change: transform, opacity;
+        transform: translateY(100%);
+        opacity: 0;
+        animation: 1s cubic-bezier(0.215, 0.61, 0.355, 1) 0.5s 1 slideInFromBottomWithOpacity forwards;
+        border-radius: 5px;
+    }
+
+    #splash-gradient-two {
+        width: 100%;
+        height: 100%;
+        will-change: transform;
+        background: #C779D0;
+    }
+
+    #splash-gradient-in{
+        width: 100%;
+        height: 100%;
+        animation: 1s cubic-bezier(0.215, 0.61, 0.355, 1) 1s 1 slideInFromBottom forwards;
+        background: #FEAC5E; /* fallback for old browsers */
+        background: -webkit-linear-gradient(to top, #FEAC5E,  #C779D0); /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to top, #FEAC5E, #C779D0); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    }
+
+    #svg-two {
+        width: 100%;
+        height: 20%;
+        /* set height to pixels if you want angle to change with screen width */
     }
 
     .title {
@@ -56,7 +141,7 @@
         flex-direction: row;
         justify-content: flex-start;
         position: relative;
-        top: 50%;
+        top: 32%;
         margin-left: 16px;
         color: #FFF;
         font-family: "Bebas Neue", sans-serif;
@@ -66,20 +151,11 @@
         font-weight: 500;
         line-height: 26.4px;
         opacity: 0;
-        animation: 1s cubic-bezier(0.215, 0.61, 0.355, 1) 1s 1 slideInFromTopWithOpacity forwards;
+        animation: 2s cubic-bezier(0.215, 0.61, 0.355, 1) 1s 1 slideInFromTopWithOpacity forwards;
     }
 
-    #qualities{
+    #qualities {
         margin-left: 16px;
-    }
-
-    svg {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 20vh;
-        /* set height to pixels if you want angle to change with screen width */
     }
 
     @keyframes slideInFromTop {
@@ -91,9 +167,29 @@
         }
     }
 
+    @keyframes slideInFromBottom {
+        0% {
+            transform: translateY(100%);
+        }
+        100% {
+            transform: translateY(0);
+        }
+    }
+
     @keyframes slideInFromTopWithOpacity {
         0% {
-            transform: translateY(-200%);
+            transform: translateY(-100%);
+            opacity: 0;
+        }
+        100% {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes slideInFromBottomWithOpacity {
+        0% {
+            transform: translateY(100%);
             opacity: 0;
         }
         100% {
